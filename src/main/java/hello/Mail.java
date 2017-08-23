e243555f9fb2a4acf1a24a3cc418dbc45722d2c5
Ashwin Ramesh <ashwin2007ray@gmail.com>
2017-08-23 10:08:59 +1000

    Compile regex once

--- src/main/java/hello/Mail.java
+++ src/main/java/hello/Mail.java
@@ -11,8 +11,9 @@ public class Mail {
     public String subject;
     public String message;
 
+    private static Pattern regexPattern = Pattern.compile("^[(a-zA-Z-0-9-\\_\\+\\.)]+@[(a-z-A-z)]+\\.[(a-zA-z)]{2,3}$");
+
     public Boolean validateEmail(String emailAddress) {
-        Pattern regexPattern = Pattern.compile("^[(a-zA-Z-0-9-\\_\\+\\.)]+@[(a-z-A-z)]+\\.[(a-zA-z)]{2,3}$");
         Matcher regMatcher = regexPattern.matcher(emailAddress);
         if (regMatcher.matches()) {
             return true;
